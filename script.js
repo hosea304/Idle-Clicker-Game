0d2f4d0752745d217762dcbba5b864bb6b200618
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-04-05 13:54:23 +0700

    menambahkan item

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let name = "Bunny";
 let cost = 15;
 let tambah = 0;
 
+let item1Cost = 50;
+let item1Level = 0;
+let item1Add = 1;
+
+let item2Cost = 100;
+let item2Level = 0;
+let item2Add = 5;
+
 function buyButton(){
     if(score >= cost)
     {
@@ -15,11 +23,54 @@ function buyButton(){
         document.getElementById("cost").innerHTML = cost;
         document.getElementById("tambah").innerHTML = tambah;
     }
+    else
+    {
+        alert("Score tidak cukup untuk membeli");
+        return;
+    }
 }
 
 function addToScore(amount){
     score+=amount;
     document.getElementById("score").innerHTML = score;
+}
+
+function buyItem1(){
+    if(score >= item1Cost)
+    {
+        score-=item1Cost;
+        item1Level++;
+        item1Add = Math.round(item1Add * 1.1);
+        item1Cost = Math.round(item1Cost * 1.1);
+        
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("item1Cost").innerHTML = item1Cost;
+        document.getElementById("item1Level").innerHTML = item1Level;
+    }
+    else
+    {
+        alert("Score tidak cukup untuk membeli");
+        return;
+    }
+}
+
+function buyItem2(){
+    if(score >= item2Cost)
+    {
+        score-=item2Cost;
+        item2Level++;
+        item2Add = Math.round(item2Add * 1.1);
+        item2Cost = Math.round(item2Cost * 1.1);
+        
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("item2Cost").innerHTML = item2Cost;
+        document.getElementById("item2Level").innerHTML = item2Level;
+    }
+    else
+    {
+        alert("Score tidak cukup untuk membeli");
+        return;
+    }
 }
 
 setInterval(function(){
