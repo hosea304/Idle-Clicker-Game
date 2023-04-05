61564f24628d8c8835d0326b6e1014d01557e034
hosea304 <hoseahose63@gmail.com>
2023-04-05 13:25:28 +0700

    tambah otomatis dan beli buttom

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let score = 0;
 let name = "Bunny";
 
+let cost = 15;
+let tambah = 0;
+
+function buyButton(){
+    if(score >= cost)
+    {
+        score-=cost;
+        tambah += 1;
+        cost = Math.round(cost * 1.15);
+        
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("cost").innerHTML = cost;
+        document.getElementById("tambah").innerHTML = tambah;
+    }
+}
+
 function addToScore(amount){
     score+=amount;
     document.getElementById("score").innerHTML = score;
 }
+
+setInterval(function(){
+    score+=tambah;
+    document.getElementById("score").innerHTML = score;
+},1000);
