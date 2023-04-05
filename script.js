9768464eb5ab324335925f722e7bdeecec1cb53e
itsPrajna <88642463+Prjnaa@users.noreply.github.com>
2023-04-06 00:50:09 +0700

    bug fix

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let score = 0;
-let cost = 15;
-
-let tambahClick = 1;
-let tambahIdle = 0;
-
-let charlvl = 1;
-let item1Cost = 50;
-let item1Level = 0;
-
-let item2Cost = 100;
-let item2Level = 0;
-
-let multiplier = 0;
-
 $(window).on("load", function () {
+  let score = 0;
+  let cost = 15;
+
+  let tambahClick = 1;
+  let tambahIdle = 0;
+
+  let charlvl = 1;
+  let item1Cost = 50;
+  let item1Level = 0;
+
+  let item2Cost = 100;
+  let item2Level = 0;
+
+  let multiplier = 0;
+  
   if (
     "currCharlvl" in localStorage != null ||
     "currCharlvl" in localStorage != NaN
